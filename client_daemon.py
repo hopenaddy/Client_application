236763d8eba9afcdcf94861d2654128e1932add2
hopenaddy <na_5_ve@ukr.net>
2014-10-31 12:52:36 +0200

    Update client_daemon.py

--- client_daemon.py
+++ client_daemon.py
@@ -2,26 +2,30 @@ import argparse
 import requests
 import pycurl
 from time import sleep
- 
+
+status = requests.get('http://ukr.net/') 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--foreground",  action="store_true")
 args = parser.parse_args()
 
-def do_job():
-    info = requests.get('http://ukr.net/')
-    print info
+def getstatus()
+    print status
 def getUserData():
 	c = pycurl.Curl()
 	c.setopt(pycurl.URL, 'https://ukr.net')
 	c.setopt(pycurl.HTTPHEADER, ['Accept: application/json'])
 	c.setopt(pycurl.USERPWD, 'username:userpass')
 	c.perform()
-if args.foreground:
-    print "Foreground option"
-    while 1:
-    	do_job()
-    	getUserData()
-    	sleep(5)
-else:
-    print "Foreground option was not passed, going to be a daemon"
-    do_job()
+	    
+if __name__ == "__main__":
+	if args.foreground:
+    		print "Foreground option"
+    		while 1:
+    		getstatus()
+    		getUserData()
+    		sleep(5)
+	else:
+    	print "Foreground option was not passed, going to be a daemon"
+    	getstatus()
+
+	
