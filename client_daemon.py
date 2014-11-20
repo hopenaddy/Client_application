638dac71cb0ccf9d1ca50c40b9b906e3b6128549
Dovbush <dovbush.it@gmail.com>
2014-11-20 23:31:43 +0200

    modified

--- client_daemon.py
+++ client_daemon.py
@@ -4,29 +4,36 @@ import pycurl
 from daemonize import Daemonize
 from time import sleep
 
-status = requests.get('http://ukr.net/') 
+URL_ADDRESS = 'http://ukr.net/'
+
+status = requests.get(URL_ADDRESS) 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--foreground",  action="store_true")
 args = parser.parse_args()
 
 def getstatus():
     return status
+
 def getUserData():
     c = pycurl.Curl()
-    c.setopt(pycurl.URL, 'https://ukr.net')
+    c.setopt(pycurl.URL, URL_ADDRESS)
     c.setopt(pycurl.HTTPHEADER, ['Accept: application/json'])
     c.setopt(pycurl.USERPWD, 'username:userpass')
     c.perform()
+
 def test():
     while 1:
+        print getstatus()
         getUserData()
         sleep(5)
 
 if __name__ == "__main__":
+    
     if args.foreground:
         pid = "/tmp/test.pid"
         daemon = Daemonize(app="test_app", pid=pid, action=test)
-        daemon.start();
+        daemon.start()
             
     else:
+        print getstatus()
         getUserData()
