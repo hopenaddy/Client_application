f57fb6e1e0b3a9df8bf9b770ca7a7a67c9c3d6e7
hopenaddy <na_5_ve@ukr.net>
2014-12-10 21:28:43 +0200

    add auth on http://client.lv128.tk/login

--- client_daemon.py
+++ client_daemon.py
@@ -1,11 +1,11 @@
 import argparse
-import os
 import requests
-import pycurl
 from daemonize import Daemonize
+import json
 from time import sleep
+import getpass
 
-URL_ADDRESS = "http://ukr.net"
+URL_ADDRESS = "http://client.lv128.tk/login"
 PID_FILE = "/tmp/test.pid"
 
 new_status = requests.get(URL_ADDRESS) 
@@ -16,11 +16,13 @@ args = parser.parse_args()
 def getStatus():
     return new_status.status_code
     
-def getUserData():
-    r = requests.get(URL_ADDRESS)
-    head = r.headers
-    for key, value in head.iteritems() :
-        print key, value
+def auth():
+    login = raw_input('Login:')
+    password = getpass.getpass()
+    payload = {'Username': login,'data':password}
+    headers = {'content-type': 'application/json'}
+    r = requests.post(URL_ADDRESS, data=json.dumps(payload), headers=headers)
+    print r.content
     
 def mytest():
     while 1:
@@ -30,8 +32,8 @@ def mytest():
 
 if __name__ == "__main__":
     if args.foreground:
-        daemon = Daemonize(app="test_app", pid=PID_FILE, action=mytest)
-        daemon.start()
+         daemon = Daemonize(app="test_app", pid=PID_FILE, action=mytest)
+         daemon.start()
     else:
         print getStatus()
-        getUserData()
+        auth()
